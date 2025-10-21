4d41128d09285efaf72445ed39ef78e3c862e4c3
sudais122 <khansb17798@gmail.com>
2025-10-21 22:32:19 +0500

    Color pannel

---
+++ Image Gallery/images.js
@@ -0,0 +1,39 @@
+const textcontainer = document.querySelectorAll('.container')
+const pic = document.querySelectorAll('.pic');
+const text = document.querySelector('.text')
+
+pic.forEach( (pics) => {
+    pics.addEventListener('mouseover', () =>{
+        pics.style.width = '30%';
+        pics.style.transition = 'all 0.4s ease';
+    })
+
+    pics.addEventListener('mouseleave' , ()=>{
+        pics.style.width = '20%';
+    })
+});
+
+const pics = document.querySelectorAll('.pic');
+
+pics.forEach(pic => {
+  const top = pic.querySelector('.top-text');
+  const bottom = pic.querySelector('.bottom-text');
+
+  pic.addEventListener('mouseover', () => {
+    pic.style.width = '30%';
+    pic.style.transition = 'all 0.4s ease';
+
+    top.style.transform = 'translateY(0rem)';
+    top.style.transition = 'transform 0.5s ease';
+
+    bottom.style.transform = 'translateY(0rem)';
+    bottom.style.transition = 'transform 0.5s ease';
+  });
+
+  pic.addEventListener('mouseleave', () => {
+    pic.style.width = '20%';
+
+    top.style.transform = 'translateY(-50rem)';
+    bottom.style.transform = 'translateY(50rem)';
+  });
+});
